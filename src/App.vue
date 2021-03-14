a35047aa37a87a051312cd203bf7d416e3d89703
bakir1 <bakir_cutuk@hotmail.com>
2021-03-14 14:58:09 +0100

    add

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,43 @@
 <template>
-<fragme>
- <h1>Heros</h1>
-  <ul>
-    <li v-for:="num in 10">{{ num }}</li>
-  </ul>
-</fragme>
+ <fragment>
+  <h1>Heros</h1>
+   <ul>
+      <li v-for:="(hero, index) in Heros" :key="index">
+      {{ hero.name }} 
+      </li>
+   </ul>
+   <form @submit.prevent="addHero">
+     <input v-model="newHero" placeholder="Type Hero Name" />
+     <button type="submit">Add Hero</button>
+   </form>
+ </fragment>
 </template>
 
 <script>
 export default {
+  methods: {
+    addHero(){
+    if(this.newHero != ""){
 
-}
+    
+    this.Heros.push({ name: this.newHero });
+    this.newHero = "";
+     }
+    },
+  },
+  removeHero(){
+
+  },
+  data() {
+    return {
+      newHero: "",
+      Heros: [
+        {name: "SuperWomen"},
+        {name: "Flash"},
+        {name: "Batman"},
+        {name: "Arrow"},
+      ],
+    };
+  },
+};
 </script>
